e4351d736d5f9df0a400f2f8c028411f4c7565f7
Davi Marcilio <davi.burato@criciuma.unimedsc.com.br>
2024-08-27 14:02:16 -0300

    🐛Bug: Routing navigation

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit, SimpleChanges } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 
 interface FormGroupProps {
-  document: FormControl<string | null>
-  password: FormControl<string | null>
+  document: FormControl<string | null>;
+  password: FormControl<string | null>;
 }
 
 @Component({
@@ -13,56 +20,52 @@ interface FormGroupProps {
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-
   formSignIn!: FormGroup<FormGroupProps>;
 
-  isShowPassword = false
+  isShowPassword = false;
 
-  submitted = false
+  submitted = false;
 
-
-
-  constructor(private alertController: AlertController) {
-
-
-  }
+  constructor(
+    private alertController: AlertController,
+    private router: Router
+  ) {}
 
   onChangeShowPassword() {
-    this.isShowPassword = !this.isShowPassword
+    this.isShowPassword = !this.isShowPassword;
   }
 
-  public isFetching: boolean = false
-
+  public isFetching: boolean = false;
 
   ngOnInit() {
-
     this.formSignIn = new FormGroup({
-      document: new FormControl('', ([Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern(/^[0-9]+$/)])),
-      password: new FormControl('', ([Validators.required, Validators.minLength(5), Validators.maxLength(255)])),
-    }
-    )
-
-
-
-
+      document: new FormControl('', [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        Validators.pattern(/^[0-9]+$/),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(255),
+      ]),
+    });
   }
 
-
-
   async onSignIn() {
-
     try {
-      this.isFetching = true
+      this.isFetching = true;
       await new Promise((resolve) => {
-        setTimeout(resolve, 2000)
-      })
+        setTimeout(resolve, 2000);
+      });
       const alert = await this.alertController.create({
         message: 'Login realizado',
-        cssClass: 'alert-success'
+        cssClass: 'alert-success',
       });
 
       await alert.present();
+      await this.router.navigate(['tabs']);
     } catch (error) {
       console.error(error);
 
@@ -70,15 +73,8 @@ export class LoginPage implements OnInit {
         message: String(error),
       });
       await alert.present();
-
     } finally {
-      this.isFetching = false
+      this.isFetching = false;
     }
-
-
   }
-
-
-
-
 }
